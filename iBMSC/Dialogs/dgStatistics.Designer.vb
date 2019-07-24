b5ea4272f25cf2e69aed8a73cb21848f56592469
Bluvel <naka4959@kcn.ne.jp>
2019-07-25 06:16:54 +0900

    fix Statistics

--- iBMSC/Dialogs/dgStatistics.Designer.vb
+++ iBMSC/Dialogs/dgStatistics.Designer.vb
@@ -27,7 +27,7 @@ Partial Class dgStatistics
         Me.lIcon = New System.Windows.Forms.Label
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel
         Me.Label2 = New System.Windows.Forms.Label
-        Me.Label15 = New System.Windows.Forms.Label
+        Me.Label16 = New System.Windows.Forms.Label
         Me.Label1 = New System.Windows.Forms.Label
         Me.Label6 = New System.Windows.Forms.Label
         Me.Label7 = New System.Windows.Forms.Label
@@ -38,6 +38,7 @@ Partial Class dgStatistics
         Me.Label12 = New System.Windows.Forms.Label
         Me.Label13 = New System.Windows.Forms.Label
         Me.Label14 = New System.Windows.Forms.Label
+        Me.Label15 = New System.Windows.Forms.Label
         Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -46,7 +47,7 @@ Partial Class dgStatistics
         Me.OK_Button.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.OK_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.OK_Button.FlatStyle = System.Windows.Forms.FlatStyle.System
-        Me.OK_Button.Location = New System.Drawing.Point(381, 188)
+        Me.OK_Button.Location = New System.Drawing.Point(381, 212)
         Me.OK_Button.Name = "OK_Button"
         Me.OK_Button.Size = New System.Drawing.Size(78, 27)
         Me.OK_Button.TabIndex = 0
@@ -62,7 +63,7 @@ Partial Class dgStatistics
         '
         'TableLayoutPanel1
         '
-        Me.TableLayoutPanel1.ColumnCount = 7
+        Me.TableLayoutPanel1.ColumnCount = 8
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle)
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
@@ -71,17 +72,18 @@ Partial Class dgStatistics
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
         Me.TableLayoutPanel1.Controls.Add(Me.Label2, 5, 0)
-        Me.TableLayoutPanel1.Controls.Add(Me.Label15, 4, 0)
-        Me.TableLayoutPanel1.Controls.Add(Me.Label1, 0, 6)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label16, 4, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label1, 0, 7)
         Me.TableLayoutPanel1.Controls.Add(Me.Label6, 0, 1)
         Me.TableLayoutPanel1.Controls.Add(Me.Label7, 0, 2)
         Me.TableLayoutPanel1.Controls.Add(Me.Label8, 0, 3)
         Me.TableLayoutPanel1.Controls.Add(Me.Label9, 0, 4)
         Me.TableLayoutPanel1.Controls.Add(Me.Label10, 0, 5)
-        Me.TableLayoutPanel1.Controls.Add(Me.Label11, 1, 0)
-        Me.TableLayoutPanel1.Controls.Add(Me.Label12, 2, 0)
-        Me.TableLayoutPanel1.Controls.Add(Me.Label13, 3, 0)
-        Me.TableLayoutPanel1.Controls.Add(Me.Label14, 4, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label11, 0, 6)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label12, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label13, 2, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label14, 3, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label15, 4, 0)
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(64, 12)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         Me.TableLayoutPanel1.RowCount = 7
@@ -92,7 +94,8 @@ Partial Class dgStatistics
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(387, 164)
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(387, 188)
         Me.TableLayoutPanel1.TabIndex = 12
         '
         'Label2
@@ -107,23 +110,24 @@ Partial Class dgStatistics
         Me.Label2.Text = "Total"
         Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'Label15
+        'Label16
         '
-        Me.Label15.AutoSize = True
-        Me.Label15.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label15.Location = New System.Drawing.Point(274, 0)
-        Me.Label15.Margin = New System.Windows.Forms.Padding(0)
-        Me.Label15.Name = "Label15"
-        Me.Label15.Size = New System.Drawing.Size(56, 25)
-        Me.Label15.TabIndex = 15
-        Me.Label15.Text = "Errors"
-        Me.Label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label16.AutoSize = True
+        Me.Label16.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label16.Location = New System.Drawing.Point(274, 0)
+        Me.Label16.Margin = New System.Windows.Forms.Padding(0)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(56, 25)
+        Me.Label16.TabIndex = 15
+        Me.Label16.Text = "Errors"
+        Me.Label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Label1
         '
         Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(16, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
         Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label1.Location = New System.Drawing.Point(0, 140)
+        Me.Label1.Location = New System.Drawing.Point(0, 163)
         Me.Label1.Margin = New System.Windows.Forms.Padding(0)
         Me.Label1.Name = "Label1"
         Me.Label1.Padding = New System.Windows.Forms.Padding(10, 0, 0, 0)
@@ -170,7 +174,7 @@ Partial Class dgStatistics
         Me.Label8.Padding = New System.Windows.Forms.Padding(10, 0, 0, 0)
         Me.Label8.Size = New System.Drawing.Size(50, 23)
         Me.Label8.TabIndex = 13
-        Me.Label8.Text = "A1-A8"
+        Me.Label8.Text = "SCROLL"
         Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'Label9
@@ -183,7 +187,7 @@ Partial Class dgStatistics
         Me.Label9.Padding = New System.Windows.Forms.Padding(10, 0, 0, 0)
         Me.Label9.Size = New System.Drawing.Size(50, 23)
         Me.Label9.TabIndex = 13
-        Me.Label9.Text = "D1-D8"
+        Me.Label9.Text = "A1-A8"
         Me.Label9.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'Label10
@@ -197,63 +201,76 @@ Partial Class dgStatistics
         Me.Label10.Padding = New System.Windows.Forms.Padding(10, 0, 0, 0)
         Me.Label10.Size = New System.Drawing.Size(50, 23)
         Me.Label10.TabIndex = 13
-        Me.Label10.Text = "BGM"
+        Me.Label10.Text = "D1-D8"
         Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'Label11
         '
         Me.Label11.AutoSize = True
         Me.Label11.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label11.Location = New System.Drawing.Point(50, 0)
+        Me.Label11.Location = New System.Drawing.Point(0, 140)
         Me.Label11.Margin = New System.Windows.Forms.Padding(0)
         Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(56, 25)
+        Me.Label11.Padding = New System.Windows.Forms.Padding(10, 0, 0, 0)
+        Me.Label11.Size = New System.Drawing.Size(50, 23)
         Me.Label11.TabIndex = 13
-        Me.Label11.Text = "Short"
-        Me.Label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label11.Text = "BGM"
+        Me.Label11.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'Label12
         '
         Me.Label12.AutoSize = True
         Me.Label12.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label12.Location = New System.Drawing.Point(106, 0)
+        Me.Label12.Location = New System.Drawing.Point(50, 0)
         Me.Label12.Margin = New System.Windows.Forms.Padding(0)
         Me.Label12.Name = "Label12"
         Me.Label12.Size = New System.Drawing.Size(56, 25)
         Me.Label12.TabIndex = 13
-        Me.Label12.Text = "Long"
+        Me.Label12.Text = "Short"
         Me.Label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Label13
         '
         Me.Label13.AutoSize = True
         Me.Label13.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label13.Location = New System.Drawing.Point(162, 0)
+        Me.Label13.Location = New System.Drawing.Point(106, 0)
         Me.Label13.Margin = New System.Windows.Forms.Padding(0)
         Me.Label13.Name = "Label13"
         Me.Label13.Size = New System.Drawing.Size(56, 25)
         Me.Label13.TabIndex = 13
-        Me.Label13.Text = "LnObj"
+        Me.Label13.Text = "Long"
         Me.Label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Label14
         '
         Me.Label14.AutoSize = True
         Me.Label14.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Label14.Location = New System.Drawing.Point(218, 0)
+        Me.Label14.Location = New System.Drawing.Point(162, 0)
         Me.Label14.Margin = New System.Windows.Forms.Padding(0)
         Me.Label14.Name = "Label14"
         Me.Label14.Size = New System.Drawing.Size(56, 25)
         Me.Label14.TabIndex = 13
-        Me.Label14.Text = "Hidden"
+        Me.Label14.Text = "LnObj"
         Me.Label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label15.Location = New System.Drawing.Point(218, 0)
+        Me.Label15.Margin = New System.Windows.Forms.Padding(0)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(56, 25)
+        Me.Label15.TabIndex = 13
+        Me.Label15.Text = "Hidden"
+        Me.Label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'dgStatistics
         '
         Me.AcceptButton = Me.OK_Button
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
         Me.CancelButton = Me.OK_Button
-        Me.ClientSize = New System.Drawing.Size(482, 230)
+        Me.ClientSize = New System.Drawing.Size(482, 254)
         Me.Controls.Add(Me.TableLayoutPanel1)
         Me.Controls.Add(Me.lIcon)
         Me.Controls.Add(Me.OK_Button)
@@ -275,7 +292,7 @@ Partial Class dgStatistics
     Friend WithEvents lIcon As System.Windows.Forms.Label
     Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
     Friend WithEvents Label6 As System.Windows.Forms.Label
-    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents Label16 As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label7 As System.Windows.Forms.Label
     Friend WithEvents Label8 As System.Windows.Forms.Label
@@ -285,6 +302,7 @@ Partial Class dgStatistics
     Friend WithEvents Label12 As System.Windows.Forms.Label
     Friend WithEvents Label13 As System.Windows.Forms.Label
     Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents Label15 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
 
 End Class
